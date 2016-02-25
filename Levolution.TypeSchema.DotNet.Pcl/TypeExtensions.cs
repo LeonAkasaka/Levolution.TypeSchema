5faecef406d2cc7a9f733ca635f824366fa9e704
Leon Akasaka <leon@wisdomsoft.jp>
2016-02-25 16:36:54 +0900

    Add: TypeExtensions for logical DotNetType.

---
+++ Levolution.TypeSchema.DotNet.Pcl/TypeExtensions.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Levolution.Core.Types;
+using System.Collections.Generic;
+
+namespace Levolution.TypeSchema.DotNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class TypeExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static DotNetType ToLogicalType(this Type type)
+            => type.GetTypeInfo().ToLogicalType();
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="typeInfo"></param>
+        /// <returns></returns>
+        public static DotNetType ToLogicalType(this TypeInfo typeInfo)
+        {
+            var logicalType = typeInfo.IsClass ? CreateClassType(typeInfo)
+                : typeInfo.IsGenericParameter ? CreateParameterType(typeInfo)
+                : typeInfo.IsInterface ? CreateInterfaceType(typeInfo)
+                : (DotNetType)CreateStructType(typeInfo);
+            return logicalType;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="memberInfo"></param>
+        /// <returns></returns>
+        public static DotNetMember ToLogicalMember(this MemberInfo memberInfo)
+        {
+            var p = memberInfo as PropertyInfo;
+            if (p != null) { return p.ToLogicalMember(); }
+            return null;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="memberInfo"></param>
+        /// <returns></returns>
+        public static DotNetProperty ToLogicalMember(this PropertyInfo memberInfo)
+        {
+            var logicalType = memberInfo.DeclaringType.ToLogicalType();
+            return new DotNetProperty(memberInfo.Name, logicalType);
+        }
+
+        private static ClassType CreateClassType(TypeInfo typeInfo)
+        {
+            var parameterTypes = GetArgumentParameterTypes(typeInfo);
+            var baseType = GetBaseType(typeInfo);
+            var interfaceTypes = GetImplementedInterfaceTypes(typeInfo);
+
+            var logicalType = new ClassType(typeInfo.Name, parameterTypes, baseType, interfaceTypes);
+            foreach (var member in GetMembers(typeInfo))
+            {
+                logicalType.Members.Add(member);
+            }
+
+            return logicalType;
+        }
+
+        private static IEnumerable<DotNetMember> GetMembers(TypeInfo typeInfo)
+            => typeInfo.DeclaredMembers.Select(x => x.ToLogicalMember());
+
+        private static InterfaceType CreateInterfaceType(TypeInfo typeInfo)
+        {
+            var parameterTypes = GetArgumentParameterTypes(typeInfo);
+            var interfaceTypes = GetImplementedInterfaceTypes(typeInfo);
+            var logicalType = new InterfaceType(typeInfo.Name, parameterTypes, interfaceTypes);
+            return logicalType;
+        }
+
+        private static ParameterType CreateParameterType(TypeInfo typeInfo)
+        {
+            var logicalType = new ParameterType(typeInfo.Name);
+            return logicalType;
+        }
+
+        private static StructType CreateStructType(TypeInfo typeInfo)
+        {
+            var parameterTypes = GetArgumentParameterTypes(typeInfo);
+            var logicalType = new StructType(typeInfo.Name, parameterTypes);
+            return logicalType;
+        }
+
+        private static IEnumerable<InterfaceType> GetImplementedInterfaceTypes(TypeInfo typeInfo)
+            => typeInfo.GetAllImplementedInterfaces().Select(x => CreateInterfaceType(x.GetTypeInfo()));
+
+        private static ClassType GetBaseType(TypeInfo typeInfo)
+            => typeInfo.BaseType != null ? CreateClassType(typeInfo.BaseType.GetTypeInfo()) : null;
+
+        private static IEnumerable<ParameterType> GetArgumentParameterTypes(TypeInfo typeInfo)
+            => typeInfo.GenericTypeArguments.Select(x => CreateParameterType(x.GetTypeInfo()));
+    }
+}
