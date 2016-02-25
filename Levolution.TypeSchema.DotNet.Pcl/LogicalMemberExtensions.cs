b2d804e91dd80f1a86a53fd0c137eae804b1be31
Leon Akasaka <leon@wisdomsoft.jp>
2016-02-25 18:00:09 +0900

    Add: LogicalTypeExtensions, LogicalMemberExtensions.

---
+++ Levolution.TypeSchema.DotNet.Pcl/LogicalMemberExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Levolution.TypeSchema.DotNet
+{
+    public static class LogicalMemberExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="member"></param>
+        /// <returns></returns>
+        public static string ToCSharp(this DotNetMember member)
+            => (member as DotNetProperty)?.ToCSharp() ?? null;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="property"></param>
+        /// <returns></returns>
+        public static string ToCSharp(this DotNetProperty property)
+            => $@"public {property.PropertyType.Name} {property.Name} {{ get; set;}}";
+    }
+}
---
+++ Levolution.TypeSchema.DotNet.Pcl/LogicalTypeExtensions.cs
@@ -0,0 +1,79 @@
+﻿using Levolution.TypeSchema.LogicalType;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Levolution.TypeSchema.DotNet
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class LogicalTypeExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logicalType"></param>
+        /// <returns></returns>
+        public static string ToCSharp(this ILogicalType logicalType)
+            => (logicalType as DotNetType)?.ToCSharp();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logicalType"></param>
+        /// <returns></returns>
+        public static string ToCSharp(this DotNetType logicalType)
+            => (logicalType as ClassType)?.ToCSharp() ??
+                (logicalType as InterfaceType)?.ToCSharp() ??
+                (logicalType as StructType)?.ToCSharp();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="logicalType"></param>
+        /// <returns></returns>
+        public static string ToCSharp(this ClassType logicalType)
+            => $@"public class {logicalType.Name}{ToTypeArguments(logicalType.ParameterTypes)} : {ToClassBase(logicalType.BaseType, logicalType.Interfaces)}
+{{
+{string.Join("\n", logicalType.Members.Select(x => x.ToCSharp()))}
+}}";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="interfaceType"></param>
+        /// <returns></returns>
+        public static string ToCSharp(this InterfaceType interfaceType)
+            => $@"public interface {interfaceType.Name}{ToTypeArguments(interfaceType.ParameterTypes)} : {ToClassBase(null, interfaceType.BaseInterfaces)}
+{{
+{string.Join("\n", interfaceType.Members.Select(x => x.ToCSharp()))}
+}}";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="structType"></param>
+        /// <returns></returns>
+        public static string ToCSharp(this StructType structType)
+            => $@"public struct {structType.Name}{ToTypeArguments(structType.ParameterTypes)} : {ToClassBase(null, structType.Interfaces)}
+{{
+{string.Join("\n", structType.Members.Select(x => x.ToCSharp()))}
+}}";
+
+        private static string ToClassBase(ClassType baseType, IEnumerable<InterfaceType> interfaces)
+        {
+            var str = new StringBuilder();
+            if (baseType != null) { str.Append(baseType.Name); }
+
+            str.Append(string.Join(",", interfaces.Select(x => x.Name)));
+            return str.ToString();
+        }
+
+        private static string ToTypeArguments(IEnumerable<ParameterType> parameterTypes)
+        {
+            if (parameterTypes == null || !parameterTypes.Any()) { return ""; }
+            return $"<{string.Join(",", parameterTypes.Select(x => x.Name))}>";
+        }
+    }
+}
