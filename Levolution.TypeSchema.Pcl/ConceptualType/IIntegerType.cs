bdcedd8f80c98a7bf6f5588f0b2acd833ddfe2c6
Leon Akasaka <leon@wisdomsoft.jp>
2016-03-06 04:28:55 +0900

    Remove: Unused using.

--- Levolution.TypeSchema.Pcl/ConceptualType/IIntegerType.cs
+++ Levolution.TypeSchema.Pcl/ConceptualType/IIntegerType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Levolution.TypeSchema.ConceptualType
+﻿namespace Levolution.TypeSchema.ConceptualType
 {
     /// <summary>
     /// 
